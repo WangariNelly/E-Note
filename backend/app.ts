01cb66b84694f53a71e5baf93fcf28c54b157f54
WangariNelly <nellywangari30@gmail.com>
2024-08-27 07:13:59 +0300

    spinning the server

--- backend/app.ts
+++ backend/app.ts
@@ -2,16 +2,13 @@ import express, { Express } from 'express';
 
 const app: Express = express();
 
-
 /**
  * mount middlewares
  * enable json body parser and url encoded routes
  */
 app.use(express.json);
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const baseUrl: string = 'api/v1';
-
-
 
 export { app };
--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env' })
+dotenv.config({ path: '.env' });
 
 import { app } from './app';
 
 
 app.listen(process.env.PORT, () => {
-  console.log( " Server running on ${process.env.PORT}...")
+	console.log(
+		`Server running on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`,
+	);
 });
