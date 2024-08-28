5d9c0f39644631e1e6ef4a7254fd5aaeb0623064
WangariNelly <nellywangari30@gmail.com>
2024-08-28 13:33:19 +0300

    Swagger Documentation

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,9 @@
 import express, { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import router from './routes/health.route';
 import errorMiddleware from './middlewares/error.middleware';
+import swaggerDefinition from './swagger.json';
 
 const app: Express = express();
 
@@ -15,6 +18,13 @@ const baseUrl: string = '/api/v1';
 
 // start mount routes
 app.use(baseUrl + '/health', router);
+
+//mount swagger
+app.use(
+	baseUrl + '/swagger',
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDefinition),
+);
 //end mount routes
 
 app.use(errorMiddleware);
--- backend/controllers/health.controller.ts
+++ backend/controllers/health.controller.ts
@@ -1,23 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 import { CatchAsyncErrorsHelper } from '../helpers/catchAsyncErrors.helper';
-import { ErrorHandler } from '../helpers/errorHandler.helper'; 
+import { ErrorHandler } from '../helpers/errorHandler.helper';
 
-export const getApiHealth = CatchAsyncErrorsHelper( 
-	async(
-	req: Request,
-	res: Response,
-	next: NextFunction ) => {
-	// console.log("connected to controller")
-	if (!req.errored) {
-		const customError = next(new ErrorHandler('Health API down', 503));
-		console.log(customError);
-		return customError;
-	}  
+export const getApiHealth = CatchAsyncErrorsHelper(
+	async (req: Request, res: Response, next: NextFunction) => {
+		// console.log("connected to controller")
+		if (!req.errored) {
+			const customError = next(new ErrorHandler('Health API down', 503));
+			console.log(customError);
+			return customError;
+		}
 		return res.status(200).json({
 			success: true,
 			message: 'Api health ok!',
 			data: {},
 		});
-	});
-
+	},
+);
