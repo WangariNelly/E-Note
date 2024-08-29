7a2e9e9891cb20e6d4b5b66a0b84b5ae452ca266
WangariNelly <nellywangari30@gmail.com>
2024-08-30 00:23:52 +0300

    Documentation validation errors

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -4,9 +4,7 @@ import { registerUser } from '../controllers/auth.controller';
 import { authValidations } from '../validations/auth.validations';
 import { validationErrorsMiddleware } from '../middlewares/validationErrors.middleware';
 
-
 const auth = express.Router();
-
 
 auth.post(
 	'/register',
